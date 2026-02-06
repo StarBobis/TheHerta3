09eecfe9359f6fe5b886e5f0a3f3c192f1d3f31c
暗灵QWQ <lan357351@gmail.com>
2026-02-06 16:09:52 +0800

    feat(节点物体关联): 添加物体ID关联机制并优化节点菜单
    添加 SSMT_OT_RefreshNodeObjectIDs 操作符用于刷新节点物体ID关联
    在 SSMTNode_Object_Info 节点中添加 object_id 属性存储物体指针
    重构物体名称变更处理逻辑，使用定时器检查替代事件处理
    移除冗余的 object_rename_handler 并优化相关映射关系

--- blueprint/blueprint_node_obj.py
+++ blueprint/blueprint_node_obj.py
@@ -11,6 +11,40 @@ _picking_tree_name = None
 _is_viewing_group_objects = False
 
 
+class SSMT_OT_RefreshNodeObjectIDs(bpy.types.Operator):
+    '''刷新节点树中所有节点的物体ID关联'''
+    bl_idname = "ssmt.refresh_node_object_ids"
+    bl_label = "刷新物体ID关联"
+    bl_options = {'REGISTER', 'UNDO'}
+    
+    def execute(self, context):
+        tree = getattr(context.space_data, "edit_tree", None) or getattr(context.space_data, "node_tree", None)
+        
+        if not tree:
+            self.report({'WARNING'}, "无法获取节点树上下文")
+            return {'CANCELLED'}
+        
+        updated_count = 0
+        
+        for node in tree.nodes:
+            if node.bl_idname == 'SSMTNode_Object_Info':
+                obj_name = getattr(node, 'object_name', '')
+                obj_id = getattr(node, 'object_id', '')
+                
+                if obj_name and not obj_id:
+                    obj = bpy.data.objects.get(obj_name)
+                    if obj:
+                        node.object_id = str(obj.as_pointer())
+                        updated_count += 1
+        
+        if updated_count > 0:
+            self.report({'INFO'}, f"已更新 {updated_count} 个节点的物体ID关联")
+        else:
+            self.report({'INFO'}, "所有节点都已建立物体ID关联")
+        
+        return {'FINISHED'}
+
+
 class SSMT_OT_SelectNodeObject(bpy.types.Operator):
     '''Select this object in 3D View'''
     bl_idname = "ssmt.select_node_object"
@@ -143,9 +177,15 @@ class SSMTNode_Object_Info(SSMTNodeBase):
                 self.draw_ib = obj_name_split[0]
                 self.component = obj_name_split[1]
                 self.alias_name = obj_name_split[2]
+            
+            obj = bpy.data.objects.get(self.object_name)
+            if obj:
+                self.object_id = str(obj.as_pointer())
         else:
             self.label = "Object Info"
-    object_name: bpy.props.StringProperty(name="Object Name", default="", update=update_object_name) # type: ignore
+            self.object_id = ""
+    object_name: bpy.props.StringProperty(name="Object Name", default="", update=update_object_name)
+    object_id: bpy.props.StringProperty(name="Object ID", default="")
 
 
     draw_ib: bpy.props.StringProperty(name="DrawIB", default="") # type: ignore
@@ -432,6 +472,7 @@ class SSMT_OT_View_Group_Objects(bpy.types.Operator):
 
 
 classes = (
+    SSMT_OT_RefreshNodeObjectIDs,
     SSMT_OT_SelectNodeObject,
     SSMT_OT_StartPickObject,
     SSMT_OT_PickObjectModal,
